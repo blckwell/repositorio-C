7488991d538b4ac056d13453b2fecbe6fe0122f1
blckwell <tecwell@outlook.com>
2023-11-17 21:00:06 -0300

    update-lista_encadeada

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -3,56 +3,53 @@
 
 struct numerica
 {
-    int num;
+    int numero;
     struct numerica *proxima;
 };
 
 int main()
 {
+
     struct numerica *inicio, *lista;
     struct numerica celula;
     int digito;
 
-    inicio = NULL;
-
-    if(inicio == NULL) {
-        printf("Nao existe uma lista .. \n");
-        printf("criando lista ..\n");
-        lista = (struct numerica *)malloc(sizeof(struct numerica));
-        if(lista == NULL) {
-            printf("Erro, nao foi possivel alocar.\n");
-        }
+    lista = (struct numerica *)malloc(sizeof(struct numerica));
+    if (lista == NULL)
+    {
+        printf("Erro, nao foi possivel realizar a alocacao.\n");
     }
 
-    inicio = lista; 
+    inicio = lista;
 
-    while(digito != 0){
-        if(lista == NULL){
-            lista = (struct numerica *)malloc(sizeof(struct numerica));
-        }
+    printf("Digite o primeiro valor a lista:\n");
+    scanf("%d", &celula.numero);
 
-        printf("Digite o numero que deseja inserir na lista:\n");
-        scanf("%d", &celula.num);
+    lista->numero = celula.numero;
 
-        lista->num = celula.num;
-        lista->proxima = NULL;
+    while (digito != 0)
+    {
+        lista->proxima = (struct numerica *)malloc(sizeof(struct numerica));
         lista = lista->proxima;
 
-        printf("Deseja inserir outro valor a lista?(1) - sim  (2) - nao\n");
-        scanf("%d", &digito);
+        printf("Digite outro valor:\n");
+        scanf("%d", &celula.numero);
 
-        if(digito == 1) {
-            printf("Ok, vamos inserir mais valores entao ..\n");
-        } else {
-            printf("Certo. Valeu, flw .. fim.\n");
-        }
+        lista->numero = celula.numero;
+
+        printf("Deseja inserir mais um elemento a lista? (1) - sim ou (0) - nao\n");
+        scanf("%d", &digito);
     }
 
-    while(inicio != NULL) {
-        printf("Aqui estao os valores da lista:\n");
-        printf("%d\n\n", inicio->num);
+    lista->proxima = NULL;
+    lista = lista->proxima;
+
+    printf("Lista encadeada gerada ..\n");
+
+    while (inicio != NULL)
+    {
+        printf("%d\n", inicio->numero);
         inicio = inicio->proxima;
     }
-    
     return 0;
 }
