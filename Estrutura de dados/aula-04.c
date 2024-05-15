79c437d6d1dce275811e2c88ad4e8b23b429d4fc
Wellgomes <tecwell@outlook.com>
2024-05-15 13:58:13 -0300

    update

---
+++ Estrutura de dados/aula-04.c
@@ -0,0 +1,22 @@
+// VALORES E ENDEREÇOS DE MEMÓRIA
+
+#include <stdio.h>
+
+int main () {
+
+int a = 10;
+int b, c;
+
+printf("&a = %p, a = %d\n", &a, a);
+printf("&b = %p, b = %d\n", &b, b);
+printf("&c = %p, c = %d\n\n", &c, c);
+
+b = 20;
+c = a + b;
+
+printf("&a = %p, a = %d\n", &a, a);
+printf("&b = %p, b = %d\n", &b, b);
+printf("&c = %p, c = %d\n\n", &c, c);
+
+    return 0;
+}
