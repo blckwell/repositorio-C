ac14e7a5033936438ae0ec01442a1e6929d42d73
Wellgomes <tecwell@outlook.com>
2024-05-16 19:06:38 -0300

    update

---
+++ Estrutura de dados/aula-07.c
@@ -0,0 +1,18 @@
+//PONTEIRO PARA PONTEIRO
+
+#include <stdio.h>
+
+int main () {
+
+float z = 2.5;
+float *fp;
+
+fp = &z;
+
+printf("*&z = %.2f\n", *&z);
+printf("*fp = %.2f\n", *fp);
+printf("**&fp = %.2f\n", **&fp);
+
+
+    return 0;
+}
